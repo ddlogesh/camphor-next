aa86e912115cb9258e71c5e4d601533992bdd06c
Logesh Dinakaran <ddlogesh@gmail.com>
2025-06-13 00:55:23 +0530

    Added env variable

--- src/app/util.ts
+++ src/app/util.ts
@@ -1,3 +1,17 @@
-export function toPlural(text: string, count: number) {
+const toPlural = (text: string, count: number) => {
   return `${text}${count === 1 ? '' : 's'}`;
 }
+
+const parseJSON = (value: string, fallback: string | null = null) => {
+  try {
+    return JSON.parse(value);
+  } catch (error) {
+    console.error(error);
+    return fallback;
+  }
+};
+
+export {
+  toPlural,
+  parseJSON
+};
